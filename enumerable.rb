670af25525c8731df8bc4b80cdb92f540b3a4582
marijan <marijan@miave.dev>
2021-03-19 10:25:17 +0100

    Linting corrections

--- enumerable.rb
+++ enumerable.rb
@@ -1,6 +1,6 @@
 module Enumerable
   def my_each
-    return unless block_given?
+    return self unless block_given?
 
     for i in self
       yield i
@@ -18,7 +18,7 @@ module Enumerable
   end
 
   def my_select
-    return unless block_given?
+    return self unless block_given?
 
     array = []
     my_each { |e| array << e if yield e }
@@ -27,44 +27,37 @@ module Enumerable
   end
 
   def my_all?
-    return unless block_given?
+    return self unless block_given?
 
-    res = my_select do |e|
-      return yield e
-    end
+    res = my_select { |e| yield e }
     length == res.length
   end
 
   def my_any?
-    return unless block_given?
+    return self unless block_given?
 
-    res = my_select do |e|
-      return yield e
-    end
+    res = my_select { |e| yield e }
 
     res.length.positive?
   end
 
   def my_none?
-    return unless block_given?
+    return self unless block_given?
 
-    !my_any?
+    res = my_select { |e| yield e }
+
+    !res.length.positive?
   end
 
   def my_count(count = nil)
     return count if count
     return length unless block_given?
 
-    res = my_select.length do |n|
-      return yield n
-    end
-
-    res.length
+    my_select { |n| yield n }.length
   end
 
   def my_map(my_proc = nil)
     array = []
-
     my_each { |n| array << my_proc.call(n) } if my_proc
     my_each { |n| array << yield(n) } if block_given?
 
@@ -74,7 +67,7 @@ module Enumerable
   def my_inject(*arguments)
     case arguments.length
     when 1 then arguments.first.is_a?(Symbol) ? s = arguments.first : res = arguments.first
-    when 2 then res, s = *arguments
+    when 2 then res, s = arguments
     end
 
     res ||= 0

728b223d481598946de6e2c65615f0fa22fec3b1
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-04-13 17:49:40 +0200

    add test for my select method when block is given or not

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -1,21 +1,21 @@
 require_relative '../enumerable'
 
 describe '#my_each' do
-  describe 'when block is given' do
+  context 'when block is given' do
     it 'executes each element inside the array' do
       array = []
       %w[1,2,3,4,5].my_each { |element| array << element }
       expect(array).to eq(%w[1,2,3,4,5])
     end
   end
-  describe 'when no block is given' do
+  context 'when no block is given' do
     it 'it will return Enumerator' do
       array = [1, 2, 3, 4]
       expect(array.my_each).to be_a(Enumerator)
     end
   end
   describe 'my_each_with_index' do
-    describe 'when block is given' do
+    context 'when block is given' do
       it 'excutes each element with its index' do
         array = []
         sample = %w[a b c]
@@ -25,10 +25,32 @@ describe '#my_each' do
     end
   end
   describe 'my_each_with_index' do
-    describe 'when no block is given' do
+    context 'when no block is given' do
       it 'It will return Enumerator' do
         array = %w[a b c]
         expect(array.my_each_with_index).to be_a(Enumerator)
+      end
+    end
+  end
+  describe 'my_select' do
+    context 'when block is given' do
+      it 'return array with each element passing the block' do
+        array = []
+        sample = [1, 2, 3, 4, 5, 6]
+        sample.my_select { |elem| array << elem if elem.even? }
+        expect(array).to eq([2, 4, 6])
+      end
+    end
+  end
+  describe 'my_select' do
+    context 'when no block is given' do
+      it 'It will return Enumerater' do
+        sample = [1, 2, 3, 4, 5, 6]
+        expect(sample.my_select).to be_a(Enumerator)
+      end
+      it 'should return an empty array if block condition is not true' do
+        arr = [1, 2, 3, 4, 5]
+        expect(arr.my_select { false }).to be_empty
       end
     end
   end

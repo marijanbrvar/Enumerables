bae43cebb274654eb8ccdc8c1e8864a03db95c5a
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-04-14 01:21:22 +0200

    add test for count, map and inject method

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -127,4 +127,104 @@ describe '#my_each' do
       end
     end
   end
+
+  describe 'my_none' do
+    context 'when block is given' do
+      it 'returns true if no elements meet the condition' do
+        sample = [1, 2, 3]
+        expect(sample.my_none? { |n| n > 3 }).to eq(true)
+      end
+      it 'returns false if any elements meet the condition' do
+        sample = [1, 2, 3, 4]
+        expect(sample.my_none? { |n| n > 2 }).to eq(false)
+      end
+    end
+    context 'when parameter is given' do
+      it 'returns true if no param meet the condition' do
+        expect([Integer, nil].my_none?(String)).to eq(true)
+      end
+      it 'returns false if any param meet the condition' do
+        expect(['hello', 2].my_none?(String)).to eq(false)
+      end
+    end
+    context 'should return false if any truthy elements' do
+      it 'returns false' do
+        expect([1, true, false].my_none?).to eq false
+      end
+      it 'should return true if no truthy elements' do
+        expect([nil, false].my_none?).to eq true
+      end
+    end
+  end
+
+  describe 'my_count' do
+    context 'when block is given' do
+      it 'should return the number of the elements matching the condition' do
+        arr = [1, 2, 3]
+        expect(arr.my_count { |counter| counter > 2 }).to eq(1)
+      end
+      it 'should return the number zero if no elements matching the condition' do
+        expect([1, 2, 3].my_count { |counter| counter > 3 }).to eq(0)
+      end
+    end
+    context 'when parameter is given' do
+      it ' returns the number of the elements in block matching parameter condition' do
+        expect([1, 2, 3, 'el'].my_count(Integer)).to eq(0)
+      end
+      it ' returns the number zero if no elements in block match the parameter condition' do
+        expect([false, 'two'].my_count(Integer)).to eq(0)
+      end
+    end
+    context 'when param is not given' do
+      it 'returns array length when parameter is absent' do
+        expect(%w[a b c].my_count).to eq(3)
+      end
+      it 'returns zero for empty array' do
+        expect([].my_count).to eq(0)
+      end
+    end
+  end
+
+  describe 'my_map' do
+    context ' when block is given' do
+      it 'should return a new array after mapping all the elements in the orginal array' do
+        expect([1, 2, 3].my_map { |element| element + 2 }).to eq [3, 4, 5]
+      end
+    end
+    context 'when block is not given' do
+      it 'should return enumerator' do
+        expect([1, 2, 3].my_map).to be_a(Enumerator)
+      end
+    end
+  end
+
+  describe 'my_inject' do
+  it 'calls LocalJumpError' do
+    expect { [1, 2, 3].my_inject }.to raise_error LocalJumpError
+  end
+
+  context 'when block is given' do
+    context 'when parameter is given' do
+      it ' should return sum of all elements in array starting from parameter' do
+        expect([1, 2, 3].my_inject(5) { |sum, value| sum + value }).to eq(11)
+      end
+      context 'when parameter is not given' do
+        it ' should return sum of all elements in array' do
+          expect([1, 2, 3].my_inject { |sum, value| sum + value }).to eq(6)
+        end
+      end
+    end
+  end
+  context 'when block is not given, starting point & symbol is given' do
+    it 'should return sum of arr elements starting from param' do
+      expect([1, 2, 3].my_inject(5, :+)).to eq(11)
+    end
+  end
+  context 'when block is not given,only symbol is given' do
+    it 'should return sum of arr elements ' do
+      expect([1, 2, 3].my_inject(:+)).to eq(6)
+    end
+  end
 end
+end
+
